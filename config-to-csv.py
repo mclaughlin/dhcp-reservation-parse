18ad0edcc9be91d9627f4bbf83b03646a63c9cff
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-03-09 11:40:48 -0800

    Added second script argument for outputting to file instead of stdout

--- config-to-csv.py
+++ config-to-csv.py
@@ -5,13 +5,18 @@ This script creates a csv file from a dhcp printer reservation conf
 """
 
 import sys
+import os
 
-def parse_config(file):
-    with open(file) as fin:
+def parse_config(input_file, output_file=None):
+    if output_file and os.path.exists(output_file):
+        try:
+            os.remove(output_file)
+        except OSError as e:
+            print(e)
+            pass 
+    with open(input_file) as fin:
         for line in fin.readlines():
-
             line = line.strip()
-
             if line[:4] == 'host':
                 row = f'{line[:-2]},'
                 last_was_comment = False
@@ -23,11 +28,22 @@ def parse_config(file):
                     row += f'{line},'
                 last_was_comment = True
             elif line == '}':
-                print(row[:-1])
+                process_output(row[:-1], output_file)
             else:
                 row += f'{line},'
                 last_was_comment = False
 
+def process_output(output, output_file=None):
+    if output_file:
+        with open(output_file, 'a') as fout:
+            fout.write(f'{output.rstrip()}\n')
+    else:
+        print(output)
+
 if __name__ == '__main__':
-        parse_config(sys.argv[1:2][0])
+    arguments = sys.argv[1:]
+    if len(arguments) > 1:
+        parse_config(arguments[0], arguments[1])
+    else:
+        parse_config(arguments[0])
 
--- csv-to-config.py
+++ csv-to-config.py
@@ -6,23 +6,40 @@ a dhcp reservation printer config file.
 """
 
 import sys
+import os
 import csv
 
-def parse_csv(file):
-    with open(file) as fin:
+def parse_csv(input_file, output_file=None):
+    if output_file and os.path.exists(output_file):
+        try:
+            os.remove(output_file)
+        except OSError as e:
+            print(e)
+            pass
+    with open(input_file) as fin:
         ss = csv.reader(fin)
         for row in ss:
             for word in row:
                 if word[:4] == 'host':
-                    print(f'{word} {{')
+                    process_output(f'{word} {{', output_file)
                 elif word[:1] == '#':
                     comments = word[1:].split('#')
                     for comment in comments:
-                        print(f'\t#{comment}')
+                        process_output(f'\t#{comment}', output_file)
                 else:
-                    print(f'\t{word}')
-            print('}\n')
+                    process_output(f'\t{word}', output_file)
+            process_output('}\n', output_file)
+
+def process_output(output, output_file=None):
+    if output_file:
+        with open(output_file, 'a') as fout:
+            fout.write(f'{output}\n')
+    else:
+        print(output)
 
 if __name__ == '__main__':
-    parse_csv(sys.argv[1:2][0])
-
+    arguments = sys.argv[1:]
+    if len(arguments) > 1:
+        parse_csv(arguments[0], arguments[1])
+    else:
+        parse_csv(arguments[0])
